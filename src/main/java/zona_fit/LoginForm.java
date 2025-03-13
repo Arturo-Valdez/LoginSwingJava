0d14ddf7d7f20cbc7a34c6351ba8dd6550001731
Arturo Valdez <santiguardado27@gmail.com>
2025-03-12 19:54:49 -0600

    Proyecto Swing Login /  completado

--- src/main/java/zona_fit/LoginForm.java
+++ src/main/java/zona_fit/LoginForm.java
@@ -3,6 +3,8 @@ package zona_fit;
 import com.formdev.flatlaf.FlatDarculaLaf;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoginForm extends JFrame{
     private JPanel panelPrincipal;
@@ -12,6 +14,28 @@ public class LoginForm extends JFrame{
 
     public  LoginForm(){
         inicializarForm();
+        enviarBoton.addActionListener(e -> validar());
+    }
+
+    private void validar() {
+        /// Leer los valores
+        var usuario = this.usuarioTexto.getText();
+        var password = new String(this.passwordTexto.getPassword());
+        if("root".equals(usuario) && "admin".equals(password)){
+            mostrarMensaje("Datos correctos, bienvenido!!");
+        }else if("root".equals(usuario)){
+            mostrarMensaje("Password incorrecto");
+        } else if ("admin".equals(password)) {
+            mostrarMensaje("Usuario incorrecto");
+        }else{
+            mostrarMensaje("USUARIO Y CONTRASEÑA INCORRECTOS");
+        }
+
+    }
+
+    //PANEL DE UN RECUADRO DE TEXTO
+    private void mostrarMensaje(String mensaje) {
+        JOptionPane.showMessageDialog(this, mensaje);
     }
 
     private void inicializarForm() {
