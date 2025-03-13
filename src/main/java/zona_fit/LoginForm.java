70063360d0daa8b4867d13ee0479b77195dfe94d
Arturo Valdez <santiguardado27@gmail.com>
2025-03-12 19:39:34 -0600

    Crear objeto login Form con swing

--- src/main/java/zona_fit/LoginForm.java
+++ src/main/java/zona_fit/LoginForm.java
@@ -1,10 +1,29 @@
 package zona_fit;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
 
-public class LoginForm {
+public class LoginForm extends JFrame{
     private JPanel panelPrincipal;
     private JTextField usuarioTexto;
     private JPasswordField passwordTexto;
     private JButton enviarBoton;
+
+    public  LoginForm(){
+        inicializarForm();
+    }
+
+    private void inicializarForm() {
+        setContentPane(panelPrincipal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null); // Centramos ventana
+    }
+
+    public static void main(String[] args) {
+        FlatDarculaLaf.setup(); // Cambia look and feel a modo dark
+        LoginForm loginForm = new LoginForm();
+        loginForm.setVisible(true);
+    }
 }
